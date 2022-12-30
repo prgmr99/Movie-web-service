6e85eb04d44cd03258e0e8228237b25fa760769d
prgmr99 <johnyeom24@naver.com>
2022-12-30 23:08:47 +0900

    first commit

---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+
+function Button({text}) {
+    return (
+        <button className={styles.btn}>
+            {text}
+        </button>
+     );
+}
+
+Button.prototype = {
+    text:PropTypes.string.isRequired, 
+}
+// App.js에서 Button을 가져올 수 있도록 하고 싶기 때문에 아래의 코드 작성.
+export default Button;
