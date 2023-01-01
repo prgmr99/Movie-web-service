e4209310e521512b95449d266a2bbea8ee477a16
prgmr99 <johnyeom24@naver.com>
2023-01-01 18:42:46 +0900

    useState with Array

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState(""); 
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => { setKeyword(event.target.value) };
-  console.log("I run all the time");
-  useEffect(() => {
-    console.log("CALL THE API...");
-  }, []);
-  useEffect(() => {
-    if(keyword !== "" && keyword.length > 4) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'counter' & 'keyword' changes");
-  }, [counter, keyword]);
-  return (
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
+    const [toDo, setTodo] = useState("");
+    const [toDos, setTodos] = useState([]);
+    const onChange = (event) => setTodo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(toDo);
+        if(toDo === "") {
+            return;
+        }
+        // 수정하는 함수를 사용할 때 2가지 방법(하나는 함수를 사용, 다른 하나는 직접 값을 대입)
+        // 첫 번째 방법은 현재 state을 계산하거나 새로운 state을 만드는데 사용할 수 있게된다.
+        setTodos(currentArray => [toDo, ...currentArray]);
+        setTodo("");
+    };
+    console.log(toDos);
+    return <div>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+            <button>Add To Do</button>
+        </form>
+        <hr/>
+        <ul>
+            {toDos.map((item,index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+    </div>;
 }
 
 export default App;
---
+++ src/useEffectTest.js
@@ -0,0 +1,34 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState(""); 
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => { setKeyword(event.target.value) };
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 4) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter' & 'keyword' changes");
+  }, [counter, keyword]);
+  return (
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
