878686ea2ff68e84e7c1fcd257350ccce7eefa05
prgmr99 <johnyeom24@naver.com>
2023-01-01 22:46:20 +0900

    [Mod] Coin Converter

--- src/CoinTrackerAndConverter.js
+++ src/CoinTrackerAndConverter.js
@@ -8,7 +8,6 @@ function App() {
     const onChange = (event) => setUsd(event.target.value);
     const onSubmit = (event) => {
         event.preventDefault();
-        
         if(setUsd === 0) {
             return;
         }
@@ -25,15 +24,25 @@ function App() {
             }); 
     }, [])
     return (<div>
-        <h1>The Coins! ({coins.length})</h1>
-        {loading ? <strong>Loading...</strong> :  null}
+        <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
         <form onSubmit={onSubmit}>
-            <input value={usd} onChange={onChange} type="number" placeholder="Input USD"/>
+            <input 
+                value={usd} 
+                onChange={onChange} 
+                type="number" 
+                placeholder="Input USD"
+            />
         </form>
+        {loading ? <strong>Loading...</strong> : 
+        <select>
+            {coins.map((coin, index) => (
+                <option key={index}>
+                    {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} - ${usd / coin.quotes.USD.price} {coin.symbol} 
+                </option>
+            ))}
+        </select>}
+        
         <h3>{btc}</h3>
-        <ul>
-            {coins.map((coin, index) => <li key={index}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} </li>)}
-        </ul>
     </div>);
 }
 
