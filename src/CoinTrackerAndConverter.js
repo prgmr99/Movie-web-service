715baed91950bc98776d03aeb74eb9732649d1dd
prgmr99 <johnyeom24@naver.com>
2023-01-01 22:58:11 +0900

    [Mod] Coin Converter

--- src/CoinTrackerAndConverter.js
+++ src/CoinTrackerAndConverter.js
@@ -4,14 +4,10 @@ function App() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
     const [usd, setUsd] = useState(0);
-    const [btc, convertToBtc] = useState(0);
+    const [amount, setAmount] = useState(0);
     const onChange = (event) => setUsd(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if(setUsd === 0) {
-            return;
-        }
-        convertToBtc((current) => usd/(current));
+    const onSelect = (event) => {
+        setAmount(event.target.value);
         setUsd(0);
     }
     useEffect(() => {
@@ -20,29 +16,31 @@ function App() {
             .then((json) => {
                 setCoins(json);
                 setLoading(false);
-                convertToBtc(json[0].quotes.USD.price);
+                //convertToBtc(json[0].quotes.USD.price);
             }); 
     }, [])
     return (<div>
         <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-        <form onSubmit={onSubmit}>
-            <input 
-                value={usd} 
-                onChange={onChange} 
-                type="number" 
-                placeholder="Input USD"
-            />
-        </form>
+        <input 
+            value={usd} 
+            onChange={onChange} 
+            type="number" 
+            placeholder="Input USD"
+        />
         {loading ? <strong>Loading...</strong> : 
-        <select>
+        <select onChange={onSelect}>
             {coins.map((coin, index) => (
-                <option key={index}>
-                    {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} - ${usd / coin.quotes.USD.price} {coin.symbol} 
+                <option 
+                    key={index}
+                    value={coin.quotes.USD.price}
+                    id={coin.name}
+                    symbol={coin.symbol}>
+                    {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} {coin.symbol} 
                 </option>
             ))}
         </select>}
         
-        <h3>{btc}</h3>
+        <h3>{usd/amount}</h3>
     </div>);
 }
 
